dad64930c0fca6cde8b655fd1b5228c05ee7410c
phamquochuy2905 <phamquochuy2905@gmail.com>
2021-12-01 23:59:49 +0100

    bootstrap installed

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.win.css'
+import 'bootstrap/dist/js/bootstrap.min'
 
 createApp(App).use(router).mount('#app')
