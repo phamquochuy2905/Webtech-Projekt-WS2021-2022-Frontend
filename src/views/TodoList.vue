3a5f39278202ecf724c28a8ee310b380046b5296
phamquochuy2905 <phamquochuy2905@gmail.com>
2021-12-20 02:43:35 +0100

    Backend + Frontend connection works on heroku

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -32,12 +32,13 @@ export default {
     }
   },
   mounted () {
+    const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + 'api/v1/todos'
     const requestOptions = {
       method: 'GET',
       redirect: 'follow'
     }
 
-    fetch('http://localhost:8080/api/v1/todos', requestOptions)
+    fetch(endpoint, requestOptions)
       .then(response => response.json())
       .then(result => result.forEach(toDoList => {
         this.toDoLists.push(toDoList)
